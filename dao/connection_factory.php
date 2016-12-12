f26a51e8a5a0a7e4fcdaf830f8dd22dea45921ab
Karine Cardozo <karinecardozo@cdurp.com.br>
2016-12-12 12:30:40 -0200

    testando banco com servidor online

--- dao/connection_factory.php
+++ dao/connection_factory.php
@@ -1,25 +1,25 @@
 <?php
 class ConnectionFactory {
 	private $servername = "localhost";
-	private $username = "root";
-	private $password = "";
-	private $dbname = "mydb";
+	private $username = "u368612708_root";
+	private $password = "123456";
+	private $dbname = "u368612708_mydb";
 
   public $connection;
 
   function __construct (){
 	  $servername = "localhost";
-	  $username = "root";
-	  $password = "";
-	  $dbname = "mydb";
-	  
+	  $username = "u368612708_root";
+	  $password = "123456";
+	  $dbname = "u368612708_mydb";
+
     $this->connection = new mysqli ($servername, $username, $password, $dbname);
     //var_dump($connection);
     if ($this->connection->connect_error){
       error_log ("Erro na conexao com o banco de dados - ConnectionFactory".$this->connection->connect_error,0);
       throw new Exception ("Erro na conexao com o banco".$this->connection->connect_error);
 	  }
-  
+
   }
 
-}	
+}
